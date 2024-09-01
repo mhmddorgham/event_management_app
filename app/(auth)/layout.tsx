fde729246db8ebf99ea7bbdcc169aa4ead795877
mhmddorgham <mohammed.dorgham25@gmail.com>
2024-09-01 17:38:55 +0400

    updates

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <div className="flex-center min-h-screen w-full bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
